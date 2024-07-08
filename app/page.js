244f2000c52123a92bc732303f8aed92f23e6e89
Arturo <turriscontreras@gmail.com>
2024-07-08 10:15:33 -0600

    Inicializando repositorio Git

---
+++ app/page.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+    <h1>Git practice</h1>
+    <p>Este cambio fue realizado desde vs CODE</p>
+    </>
+  );
+}
